f662743dcf109bd1d829f2ace47509a3a5b6c903
janvi singh <prateeksinghkushwah0@gmail.com>
2026-09-20 16:12:55 +0530

    added WIF set-up

--- 0-bootstrap/outputs.tf
+++ 0-bootstrap/outputs.tf
@@ -8,6 +8,7 @@ output "terraform_service_account_email" {
   value       = google_service_account.terraform.email
 }
 
+
 output "github_workload_identity_provider" {
   description = "Full provider resource name for the GCP_WORKLOAD_IDENTITY_PROVIDER GitHub secret."
   value       = google_iam_workload_identity_pool_provider.github.name
